e476ec1a40c643498c01007e1112b6ef461ebbc3
Mitchell Eimermann <mitch@Mitchells-MacBook-Pro.local>
2018-05-09 16:17:13 -0500

    test geolocation messege

--- server/utils/msg.test.js
+++ server/utils/msg.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./msg');
+var {generateMessage, generateLocationMessage} = require('./msg');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -13,5 +13,19 @@ describe('generateMessage', () => {
             from,
             text
         })
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'mitch';
+        var createdAt = 123;
+        var laditude = 1;
+        var longitude = 1;
+        var url = 'https://www.google.com/maps?q=1,1';
+        var message = generateLocationMessage(from, laditude, longitude)
+        
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
     });
 });
